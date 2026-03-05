4f3739ecfa05fd43d27213b7a5b463f10b2fce70
muhammadsarmad-cmd <muhammad.sarmad@tekrevol.com>
2026-03-06 00:54:26 +0500

    vector-store-1

--- rag_project.py
+++ rag_project.py
@@ -5,6 +5,15 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
 from dotenv import load_dotenv
 
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+client = QdrantClient(url="http://localhost:6333")
+client.create_collection(
+    collection_name="test_collection",
+    vectors_config=VectorParams(size=14, distance=Distance.DOT),
+)
+
+
 load_dotenv()
 
 def extract_text_from_pdf(file_path: str) -> str:
@@ -27,6 +36,12 @@ def embed_chunks(chunks:list[str])->list[list[float]]:
     embedding_model = os.getenv("EMBEDDING_MODEL")
     response = client.embeddings.create(input=chunks, model=embedding_model)
     return [embedding.embedding for embedding in response.data]
+
+#docker run -p 6333:6333 qdrant/qdrant
+def store_in_qdrant(chunks: list[str], embeddings: list[list[float]]) -> None:
+    for chunck,embedding in zip(chunks,embeddings):
+        pass
+
 
 text = extract_text_from_pdf(r'C:\rag\file-sample_150kB.pdf')
 chunks = chunk_text(text)
