93a1ae042275b7707f67a826efb43f8338d21fe8
muhammadsarmad-cmd <muhammad.sarmad@tekrevol.com>
2026-03-06 22:52:04 +0500

    working rag

--- rag_project.py
+++ rag_project.py
@@ -4,17 +4,14 @@ from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
 from dotenv import load_dotenv
-
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, VectorParams
-client = QdrantClient(url="http://localhost:6333")
-client.create_collection(
-    collection_name="test_collection",
-    vectors_config=VectorParams(size=14, distance=Distance.DOT),
-)
-
-
+from qdrant_client.models import Distance, VectorParams ,PointStruct
 load_dotenv()
+
+
+client = OpenAI()
+embedding_model = os.getenv("EMBEDDING_MODEL") 
+qdrant_client = QdrantClient(url="http://localhost:6335")
 
 def extract_text_from_pdf(file_path: str) -> str:
 
@@ -32,20 +29,64 @@ def chunk_text(text: str) -> list[str]:
     return text_splitter.split_text(text)
 
 def embed_chunks(chunks:list[str])->list[list[float]]:
-    client = OpenAI()
-    embedding_model = os.getenv("EMBEDDING_MODEL")
     response = client.embeddings.create(input=chunks, model=embedding_model)
     return [embedding.embedding for embedding in response.data]
 
 #docker run -p 6333:6333 qdrant/qdrant
 def store_in_qdrant(chunks: list[str], embeddings: list[list[float]]) -> None:
-    for chunck,embedding in zip(chunks,embeddings):
-        pass
+    
+    if not qdrant_client.collection_exists(collection_name="docs"):
+        qdrant_client.create_collection(
+            collection_name="docs",
+            vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+        )
+    for i,(chunk,embedding) in enumerate(zip(chunks,embeddings)):
+        qdrant_client.upsert(
+            collection_name="docs",
+            points=[
+                PointStruct(id=i, vector=embedding, payload={"text":chunk})
+            ]
+        )
+
+    result = qdrant_client.get_collection(collection_name="docs")
+    return result.points_count
+
+def query(query:str,top_k:int = 2)->str:
+    
+    embedd_query = client.embeddings.create(input= query, model=embedding_model)
+    
+    
+    response = qdrant_client.query_points(
+        collection_name='docs',
+        query = embedd_query.data[0].embedding,
+        limit = top_k,
+    )
+
+    context = [point.payload.get("text") for point in response.points]
+
+    context_str = "\n\n".join(context)
+
+    ai_response = client.chat.completions.create(
+        model = "o4-mini-2025-04-16",
+        messages = [
+            {"role" : "system","content" : f"answer the question based on  the following context{context_str}"},
+            {"role" : "user","content" : query},
+        ],
+    )
+
+    return ai_response.choices[0].message.content
 
 
-text = extract_text_from_pdf(r'C:\rag\file-sample_150kB.pdf')
+
+
+    
+
+
+
+
+text = extract_text_from_pdf(r'C:\rag\CIVIL SERVANTS LEAVE RULES, 1986.pdf')
 chunks = chunk_text(text)
 embeddings = embed_chunks(chunks)
-
-print(f"Total embeddings: {len(embeddings)}")
-print(f"Vector dimensions: {len(embeddings[0])}")
+store_in_qdrant(chunks, embeddings)
+ai = query("what policy of recreation leave")
+print(ai)
